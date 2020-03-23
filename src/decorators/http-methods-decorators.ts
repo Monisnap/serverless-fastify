3d160768aa051df7f517d68e24605293a2a2e783
Jaze23 <jdauxerre.pro@gmail.com>
2020-03-23 11:36:45 +0100

    added the missing decorators

--- src/decorators/http-methods-decorators.ts
+++ src/decorators/http-methods-decorators.ts
@@ -14,3 +14,36 @@ export function Get(path: string, config?: RouteShorthandOptions) {
     );
   };
 }
+
+export function Post(path: string, config?: RouteShorthandOptions) {
+  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    Reflect.defineMetadata(
+      methodMetadataKey,
+      { method: "post", path, config } as DecoratorMetadata,
+      target,
+      propertyKey
+    );
+  };
+}
+
+export function Patch(path: string, config?: RouteShorthandOptions) {
+  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    Reflect.defineMetadata(
+      methodMetadataKey,
+      { method: "patch", path, config } as DecoratorMetadata,
+      target,
+      propertyKey
+    );
+  };
+}
+
+export function Delete(path: string, config?: RouteShorthandOptions) {
+  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    Reflect.defineMetadata(
+      methodMetadataKey,
+      { method: "delete", path, config } as DecoratorMetadata,
+      target,
+      propertyKey
+    );
+  };
+}
